41d52cd0ee0a83fb8a2ca48cbaea6fb26c489c2c
axiaobug <axiaobug@hotmail.com>
2020-06-09 16:42:16 +1000

    增加一个大循环直到输入终止符END结束,并且返回sum

--- main.py
+++ main.py
@@ -4,4 +4,18 @@ from caculator import print_
 # TODO: print_里面有两个返回值每次调用print_()函数记得作sum.
 # TODO: 写一个循环,直到输入特殊终止符比如END,终止print_()函数,两个sum的返回值也写入到同一个文件.
 if __name__ == "__main__":
-    print_()
+    print("程序开始")
+    price_before_fit, price_for_self = 0, 0
+    while True:
+        input_keyb = input("确定结束吗?END结束,任意键继续")
+        if input_keyb == "END":
+            print("总计:${}\t个人自拿${}\t本人到帐${}".format(round(price_before_fit, 3), round(price_for_self, 3),
+                                                    round(price_before_fit*0.7+price_for_self, 3)))
+            print("程序结束")
+            break
+        else:
+            price_before_fit_, price_for_self_ = print_()
+            price_before_fit += price_before_fit_
+            price_for_self += price_for_self_
+            print("计算,累加成功!")
+
