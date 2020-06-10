6eefeae5991a6dae9f07e2e31d97c3e949121710
axiaobug <axiaobug@hotmail.com>
2020-06-10 21:51:08 +1000

    基本所有价格都包含在conf里面,wt只有常见的4,5

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,24 @@
-box_price = {"REC1200": 40, "REC1500": 45, "REC1800": 50, "REC2000": 60, "REC2001": 75, "REC1200D": 50, "REC1500D": 55,
-             "REC1800D": 60, "REC2000D": 70, "REC2001D": 85, "FIL1200": 75, "FIL1500": 85, "FIL1800": 95,
-             "FIL2000": 100, "FIL2001": 110, "FIL1200D": 85, "FIL1500D": 95, "FIL1800D": 105,
-             "FIL2000D": 110, "FIL2001D": 120, "STS1200": 55, "STS1500": 60, "STS1800": 75, "STS2000": 80,
-             "STS2001": 100, "STS1200D": 65, "STS1500D": 70, "STS1800D": 85, "STS2000D": 90, "STS2001D": 110,
-             "TSO1200": 55, "TSO1500": 60, "TSO1800": 75, "TSO2000": 80, "TSO2001": 100, "UIL1200": 60, "UIL1500": 65,
-             "UIL1800": 80, "UIL2000": 85, "UIL2001": 105, "ALT1200": 45, "ALT1500": 50, "ALT1800": 60, "ALT2000": 70,
-             "ALT2001": 80, "UTS1200": 23, "UTS1500": 27, "UTS1800": 35, "UTS2000": 42, "UTS2001": 50, "UTE1200": 27,
-             "UTE1500": 35, "UTE1800": 55, "UTE2000": 60, "UTE2001": 70, "UTTI1200": 50,"UTTI1500": 60, "UTTI1800": 70,
-             "UTTI200": 80, "UTTI2001": 90}
-canopy_price = {"canopy2": 210, "canopy3": 220, "jo2": 240, "jo": 250}
+box_price = {"rec1200": 40, "rec1500": 45, "rec1800": 50, "rec2000": 60, "rec2001": 75, "rec1200D": 50, "rec1500D": 55,
+             "rec1800D": 60, "rec2000D": 70, "rec2001D": 85, "fil1200": 75, "fil1500": 85, "fil1800": 95,
+             "fil2000": 100, "fil2001": 110, "sts1200": 55, "sts1500": 60, "sts1800": 75, "sts2000": 80,
+             "sts2001": 100, "tso1200": 55, "tso1500": 60, "tso1800": 75, "tso2000": 80, "tso2001": 100, "uil1200": 60, 
+             "uil1500": 65, "uil1800": 80, "uil2000": 85, "uil2001": 105, "alt1200": 45, "alt1500": 50, "alt1800": 60, 
+             "alt2000": 70, "alt2001": 80, "uts1200": 23, "uts1500": 27, "uts1800": 35, "uts2000": 42, "uts2001": 50, 
+             "ute1200": 27, "ute1500": 35, "ute1800": 55, "ute2000": 60, "ute2001": 70, "utti1200": 50, "utti1500": 60, 
+             "utti1800": 70, "utti200": 80, "utti2001": 90, "utt1200": 40, "utt1500": 45, "utt1800": 50, "utt2000": 60, 
+             "utt2001": 70, "gb1200": 38, "hdf1200": 10, "shoebox1200": 10, "ljb1200": 105, "ljb1500": 110, "ljb1800": 115,
+             "ljb2000": 120, "ljb2001": 135, "base1200": 60, "base1500": 60, "base1800": 60, "base2000": 60, "base2001": 60}
+canopy_price = {"canopy2": 210, "canopy3": 220, "jo2": 240, "jo3": 250, "wt4": 280, "wt5": 250, "mini": 150, "dog": 150,
+                "nofloor": 150}
 accessories_internal = {"divider": 5, "U": 2, "mesh": 10, "tube": 2, "draw": 20, "fic_draw": 30, "corner_cut": 5,
-                        "door": 10, "chain": 2, "weld_rr": 20, "3_point": 2, "rubber": 2, "slider": 20, "base": 60}
+                        "door": 10, "chain": 2, "weld_rr": 20, "3_point": 2, "rubber": 2, "slider": 20, "base": 60,
+                        "strut": 5}
 accessories_external = {"mesh": 10}
-special_name1, special_name2, special_name3= ["FID", "FIS"], ["DBL"], ["DBU"]
-init_door, init_divider = ["DBL", "FID", "DBU"], ["FID", "DBU", "TSO"]
+special_name1, special_name2, special_name3 = ["fid", "fis"], ["dbl"], ["dbu"]
+init_door, init_divider, init_strut = ["dbl", "fid", "dbu"], ["fid", "dbu", "tso", "fis", "fil"], ["utt", "utti"]
 thickness = {"1200": 5, "1500": 9, "1800": 15, "2000": 15, "2001": 15}
 flat_plat = {"1200": 0, "1500": 5, "1800": 10, "2000": 10, "2001": 10}
 box_ratio, canopy_ratio = 0.775, 0.925
-series_name = ["REC", "STS", "FIL", "FIS", "UIS", "DBL", "FID", "DBU", "UIL", "ALT", "UTS", "UTTI", "UTE", "TB", "HDF",
-               "TSO"]
-canopy_name = ["canopy2", "canopy3", "jo2", "jo3"]
+series_name = ["rec", "sts", "fil", "fis", "UIS", "dbl", "fid", "dbu", "uil", "alt", "uts", "utti", "ute", "tb", "hdf",
+               "tso", "utt", "ljb", "shoebox"]
+canopy_name = ["canopy2", "canopy3", "jo2", "jo3", "wt4", "wt5", "mini", "dog", "nofloor"]
